b62a1830f883e675314f3c3da357d19812332395
Qing Bin Liang <leunghb@hotmail.com>
2023-07-22 21:03:06 -0700

    add fetch

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ console.log('main.js connected');
 
 const searchTermsInput = document.body.querySelector("#search-terms");
 
+const getMealCategories = async () => {
+    const getMealCategoriesApiURL = "https://www.themealdb.com/api/json/v1/1/categories.php"
+
+    await fetch(getMealCategoriesApiURL)
+}
 
 const handleFormInputFocus = () => {
-    console.log(`focus occurred`)
+    console.log(`focus occurred`);
+
+    getMealCategories();
 };
 
 searchTermsInput.addEventListener('focus', handleFormInputFocus);
